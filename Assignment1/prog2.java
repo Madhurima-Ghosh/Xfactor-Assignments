c40a39d9b6f926b5945e76469c6ebc2af84b53cb
Madhurima Ghosh <54733636+Madhurima-Ghosh@users.noreply.github.com>
2021-04-30 20:12:05 +0530

    Create prog2.java

---
+++ Assignment1/prog2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class program2 {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int rem = a;
+        int q = 0;
+        while(rem>=b)
+        {
+            rem = rem-b;
+            q += 1;
+        }
+        System.out.println("Quotient: "+q+" Remainder: "+rem);
+        input.close();
+    }
+}
